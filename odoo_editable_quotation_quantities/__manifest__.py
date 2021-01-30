e789ec9ac71ef7cc463b417bcaf875e026f9bf6a
Jasad Moozhiyan <jasad@technaureus.com>
2021-01-30 10:33:55 +0530

    file added

--- odoo_editable_quotation_quantities/__manifest__.py
+++ odoo_editable_quotation_quantities/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": 'Odoo Editable Quotation Quantities',
     "category": "sales",
-    "version": '14.0.0.1',
+    "version": '14.0.0.2',
     "sequence": 1,
     'summary': 'Odoo Editable Quotation Quantities',
     'website': 'http://www.technaureus.com/',
@@ -17,7 +17,7 @@
         'sale_management',
     ],
     "demo": [],
-    "data": ['views/sale_editable_quotation.xml',
+    "data": ['views/res_config_settings_views.xml',
              'views/templates.xml'
              ],
     'images': [''],
